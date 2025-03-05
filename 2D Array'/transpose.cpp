c26ab644841aaa391c57a78783fdbaaac67f5a55
bvvivek6 <vivekbv242813@gmail.com>
2025-03-05 22:07:03 +0530

    Add solution for transposing a matrix

---
+++ 2D Array'/transpose.cpp
@@ -0,0 +1,28 @@
+/*
+867. Transpose Matrix
+
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.j*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> transpose(vector<vector<int>> &matrix)
+    {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<vector<int>> transposed(cols, vector<int>(rows));
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposed;
+    }
+};
