3c064c51221f004818ce4a6b912242e02ef3e794
bvvivek6 <vivekbv242813@gmail.com>
2025-03-20 18:26:55 +0530

    Add solution for merging overlapping intervals

---
+++ Greedy/mergeIntervals.cpp
@@ -0,0 +1,45 @@
+/*
+
+56. Merge Intervals
+
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+ and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+
+        sort(intervals.begin(), intervals.end());
+
+        int index = 0;
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            if (intervals[index][1] >= intervals[i][0])
+            {
+                intervals[index][1] = max(intervals[index][1], intervals[i][1]);
+            }
+            else
+            {
+                index++;
+                intervals[index] = intervals[i];
+            }
+        }
+        intervals.resize(index + 1);
+        return intervals;
+    }
+};
