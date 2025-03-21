43a831ac03f2fcb3c0621fb9a8741a2425b967a4
bvvivek6 <vivekbv242813@gmail.com>
2025-03-21 06:36:54 +0530

    Add solution for implementing queue using stacks

---
+++ Stack/impQueUsingStk.cpp
@@ -0,0 +1,86 @@
+/*232. Implement Queue using Stacks
+
+Implement a first in first out (FIFO) queue using only two stacks.
+The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+You must use only standard operations of a stack, which means only push to top,
+ peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively.
+You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class MyQueue
+{
+private:
+    stack<int> st1, st2;
+    void transfer()
+    {
+        if (st2.empty())
+        {
+            while (!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+    }
+
+public:
+    MyQueue()
+    {
+    }
+
+    void push(int x)
+    {
+        st1.push(x);
+    }
+
+    int pop()
+    {
+        transfer();
+        int val = st2.top();
+        st2.pop();
+        return val;
+    }
+
+    int peek()
+    {
+        transfer();
+        return st2.top();
+    }
+
+    bool empty()
+    {
+        return st2.empty() && st1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
