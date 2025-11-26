4a16b246ef40a670a886adf9860d6c7eddaddb9b
bvvivek6 <vivekbv242813@gmail.com>
2025-11-26 18:15:20 +0530

    Add LC 235 solution LCA

---
+++ Trees/LowestCommonAncestor.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        int small = min(p->val, q->val);
+        int large = max(p->val, q->val);
+
+        while (root != NULL)
+        {
+            if (root->val > large)
+                root = root->left;
+            else if (root->val < small)
+                root = root->right;
+            else
+                return root;
+        }
+        return NULL;
+    }
+};
